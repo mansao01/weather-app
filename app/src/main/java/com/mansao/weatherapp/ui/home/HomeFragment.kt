f4233c6779d9f248ec265784f6aa446d44716265
mansao01 <novrilukman15@gmail.com>
2022-12-05 21:12:58 +0700

    making progress at HomeFragment.kt

--- app/src/main/java/com/mansao/weatherapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mansao/weatherapp/ui/home/HomeFragment.kt
@@ -1,12 +1,15 @@
 package com.mansao.weatherapp.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
+import com.mansao.weatherapp.R
 import com.mansao.weatherapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -31,7 +34,15 @@ class HomeFragment : Fragment() {
         homeViewModel.apply {
             searchCityWeatherData("jakarta")
             weatherResponse.observe(viewLifecycleOwner) {
-                binding.textHome.text = it.location.name
+                val iconUrl = "https:${it.current.condition.icon}"
+                Log.d("Home Fragment", iconUrl)
+                binding.apply {
+                    textHome.text = it.location.name
+                    tvTempC.text = StringBuilder(it.current.tempC.toString()).append(getString(R.string.degree_celcius))
+                    Glide.with(context!!.applicationContext)
+                        .load(iconUrl)
+                        .into(binding.ivWeather)
+                }
             }
             isLoading.observe(viewLifecycleOwner){
                 showProgressBar(it)
